7fb664629319593788ae149630547a833f56d057
Germain Carré <git@germain.sh>
2021-05-08 15:17:03 +0200

    Adding slack extra checks

--- extract_list/extract_list.py
+++ extract_list/extract_list.py
@@ -23,11 +23,13 @@ logging.basicConfig(
 
 logger = logging.getLogger('extract_list')
 
+slack_client = slack_sdk.WebClient(token=config.SLACK_API_BEARER_TOKEN)
+
 pics_mimetypes = {"image/png", "image/jpeg", "image/jpg", "image/gif"}
 
 key_mappings = {
     'Nom complet': 'fullname',
-    'Canal #bénévoles_général': 'canal',
+    'Type de personne': 'type',
     'Prénom 👀': 'firstname',
     'Nom 👀': 'lastname',
     'Identité 👀': 'identity',
@@ -111,7 +113,8 @@ def get_csv_users():
                 person_id = hashlib.sha256(csv_user['email'].encode("utf-8")).hexdigest()
                 person_id = hashlib.md5(person_id.encode("utf-8")).hexdigest()
                 csv_user['id'] = person_id
-                csv_user['benevoles_channel'] = csv_user['canal'] == 'Oui'
+                csv_user['is_benevole'] = csv_user['type'] == 'Bénévole'
+                csv_user['is_invite_special'] = csv_user['type'] == 'Invité spécial'
                 if "email" in csv_user and csv_user["email"]:
                     csv_users[csv_user["email"]] = csv_user
 
@@ -121,19 +124,93 @@ def get_csv_users():
 
 
 def get_slack_users():
-    client = slack_sdk.WebClient(token=config.SLACK_API_BEARER_TOKEN)
-    response = client.users_list(limit=500)
+    logger.info(f"Loading slack users...")
+    response = slack_client.users_list(limit=1000)
     users = response["members"]
-    response = client.conversations_members(channel=config.SLACK_CHANNEL, limit=500)
-    members_ids = response["members"]
+
+    response = slack_client.team_billableInfo()
+    users_billable_info = response["billable_info"]
+
+    channels = get_slack_channels()
+    benevoles_channel_members = channels[config.BENEVOLES_SLACK_CHANNEL]["members"]
+
     members = {}
     for user in users:
-        if "profile" in user and user["profile"]:
-            if "email" in user["profile"] and user["profile"]["email"]:
-                user["benevoles_channel"] = user["id"] in members_ids
-                user["email"] = user["profile"]["email"]
-                members[user["profile"]["email"]] = user
+        if not user["is_app_user"] and not user["is_bot"] and user["name"] != "slackbot":
+            user["billing_active"] = False
+            if user["id"] in users_billable_info:
+                user["billing_active"] = users_billable_info[user["id"]]["billing_active"]
+
+            user["is_benevole"] = user["id"] in benevoles_channel_members
+            user['is_invite_special'] = False
+            if "is_ultra_restricted" in user and user['is_ultra_restricted']:
+                user['is_invite_special'] = True
+            if "is_restricted" in user and user['is_restricted']:
+                user['is_invite_special'] = True
+
+            user["email"] = user["profile"]["email"]
+            user_all_channels = {}
+            user_public_channels = {}
+            user_private_channels = {}
+            user_benevoles_channels = {}
+            user_missing_benevoles_channels = {}
+            for channel_id, channel in channels.items():
+                if user["id"] in channel["members"]:
+                    user_all_channels[channel_id] = channel
+                if channel["is_private"]:  # private channel
+                    if channel["name"].startswith("bénévoles_"):
+                        if user["id"] in channel["members"]:
+                            user_benevoles_channels[channel_id] = channel
+                        else:
+                            user_missing_benevoles_channels[channel_id] = channel
+                    else:
+                        if user["id"] in channel["members"]:
+                            user_private_channels[channel_id] = channel
+                else:
+                    if user["id"] in channel["members"]:
+                        user_public_channels[channel_id] = channel
+            user["all_channels"] = user_all_channels
+            user["public_channels"] = user_public_channels
+            user["private_channels"] = user_private_channels
+            user["benevoles_channels"] = user_benevoles_channels
+            user["missing_benevoles_channels"] = user_missing_benevoles_channels
+            members[user["profile"]["email"]] = user
     return members
+
+
+def get_slack_channels():
+    logger.info(f"Loading slack channels list...")
+    response = slack_client.conversations_list(types="public_channel,private_channel", exclude_archived=True, limit=500)
+    r_channels = response["channels"]
+    channels = {}
+    i = 1
+    for channel in r_channels:
+        i += 1
+        channel_id = channel["id"]
+        channel_name = channel["name"]
+        logger.info(f"Loading slack channel members {channel_id} ({channel_name}) {i}/{len(r_channels) + 1}...")
+        response = slack_client.conversations_members(channel=channel_id, limit=500)
+        members = response["members"]
+        channel["members"] = members
+        channels[channel["id"]] = channel
+    return channels
+
+
+def get_slack_user_presence(user):
+    response = slack_client.users_getPresence(user=user)
+    r_channels = response["channels"]
+    channels = {}
+    i = 1
+    for channel in r_channels:
+        i += 1
+        channel_id = channel["id"]
+        channel_name = channel["name"]
+        logger.info(f"Loading slack channel members {channel_id} ({channel_name}) {i}/{len(r_channels) + 1}...")
+        response = slack_client.conversations_members(channel=channel_id, limit=500)
+        members = response["members"]
+        channel["members"] = members
+        channels[channel["id"]] = channel
+    return channels
 
 
 def handle_mention(csv_user):
@@ -188,7 +265,7 @@ def verify_pic(csv_user):
         src = 'https://www.zupimages.net/up/%s' % up.query.replace('id=', '')
 
     r = requests.get(src, headers=default_headers)
-    if r.status_code >= 200 and r.status_code <= 209 and r.headers["content-type"] in pics_mimetypes:
+    if 200 <= r.status_code <= 209 and r.headers["content-type"] in pics_mimetypes:
         return src
     else:
         logger.warning(
@@ -219,7 +296,6 @@ def get_twitter_pic(csv_user):
         if twitter_user and not twitter_user["default_profile_image"] and twitter_user["profile_image_url_https"]:
             src = re.sub(r'_normal\.', '.', twitter_user["profile_image_url_https"])
             return src
-    # no way to retreive it by scrapping now, we must use https://developer.twitter.com/en/docs/twitter-api/v1/accounts-and-users/user-profile-images-and-banners
     return None
 
 
@@ -284,36 +360,84 @@ def check_consistency(csv_users, slack_users):
     for csv_email, csv_user in csv_users.items():
         full_name = csv_user['fullname']
         email = csv_email
-        if csv_email in slack_users.keys():
-            # user from csv is on slack
-            slack_user = slack_users[csv_email]
-            csv_user["slack_id"] = slack_user["id"]
-            if slack_user['benevoles_channel'] != csv_user['benevoles_channel']:
-                is_inconsistent = True
-                logger.warning(f"{email} ({full_name}) inconsistency between slack and CSV about channel "
-                               f"CSV:{csv_user['benevoles_channel']} Slack:{slack_user['benevoles_channel']}")
-        else:
+        if csv_email not in slack_users.keys():
             # user from csv is NOT on slack
             is_inconsistent = True
             logger.warning(f"{email} ({full_name}) is on the CSV list but do not exist on slack")
+
+    for slack_email, slack_user in slack_users.items():
+        full_name = slack_user["profile"]["real_name"]
+        email = slack_email
+
+        if slack_email in csv_users.keys():
+            # user from slack is on CSV
+            csv_user = csv_users[slack_email]
+            if slack_user['is_benevole'] != csv_user['is_benevole']:
+                is_inconsistent = True
+                logger.warning(f"{email} ({full_name}) inconsistency between slack and CSV about person type bénévole :"
+                               f"CSV:{csv_user['is_benevole']} Slack:{slack_user['is_benevole']}")
+
+            if slack_user['is_invite_special'] != csv_user['is_invite_special']:
+                is_inconsistent = True
+                logger.warning(f"{email} ({full_name}) inconsistency between slack and CSV about person type invité "
+                               f"spécial : CSV:{csv_user['is_invite_special']} Slack:{slack_user['is_invite_special']}")
+        else:
+            # user from slack is NOT on CSV
+            if slack_user["is_benevole"]:
+                is_inconsistent = True
+                logger.warning(f"{email} ({full_name}) is volunteer on slack and should be in CSV")
+            else:
+                is_inconsistent = True
+                logger.warning(f"{email} ({full_name}) is on slack (but NOT volunteer) and should be in CSV")
+
+        if slack_user['is_benevole']:
+            if len(slack_user["missing_benevoles_channels"]) > 0:
+                is_inconsistent = True
+                channels_names = ", ".join([v["name"] for k, v in slack_user["missing_benevoles_channels"].items()])
+                logger.warning(
+                    f"{email} ({full_name}) is volunteer on slack but is NOT on bénévoles channels : {channels_names}")
+        elif slack_user['is_invite_special']:
+            if not (set(slack_user["all_channels"].keys())).issubset(
+                    set(config.ALLOWED_SLACK_CHANNELS_FOR_INVITE_SPECIAL)):
+                is_inconsistent = True
+                channels_names = ", ".join(
+                    [v["name"] + " (" + v["id"] + ")" for k, v in slack_user["all_channels"].items()])
+                logger.warning(f"{email} ({full_name}) is invite special BUT on NOT ALLOWED CHANNELS. "
+                               f"Person is on channels : {channels_names}")
+        else:
+            if len(slack_user["private_channels"]) > 0:
+                is_inconsistent = True
+                channels_names = ", ".join([v["name"] for k, v in slack_user["private_channels"].items()])
+                logger.warning(
+                    f"{email} ({full_name}) is NOT volunteer on slack but is on private channels : {channels_names}")
+
     for slack_email, slack_user in slack_users.items():
         full_name = slack_user["profile"]["real_name"]
         email = slack_email
         if slack_email in csv_users.keys():
             # user from slack is on CSV
             csv_user = csv_users[slack_email]
-            if slack_user['benevoles_channel'] != csv_user['benevoles_channel']:
-                is_inconsistent = True
-                logger.warning(f"{email} ({full_name}) inconsistency between slack and CSV about channel "
-                               f"CSV:{csv_user['benevoles_channel']} Slack:{slack_user['benevoles_channel']}")
-        else:
-            # user from slack is NOT on CSV
-            if slack_user["benevoles_channel"]:
-                is_inconsistent = True
-                logger.warning(f"{email} ({full_name}) is volunteer on slack and should be in CSV")
+            if not slack_user['is_benevole'] and not slack_user['is_invite_special'] and not slack_user["deleted"]:
+                if "is_invited_user" not in slack_user:
+                    if slack_user['is_benevole'] == csv_user['is_benevole'] and slack_user['is_invite_special'] == \
+                            csv_user['is_invite_special']:
+                        if len(slack_user["private_channels"]) <= 0:
+                            logger.info(f"[INFO] {email} ({full_name}) is not benevole nor invite special")
+            if slack_user['is_benevole'] and not slack_user['billing_active'] and not slack_user["deleted"]:
+                channels_names = ", ".join([v["name"] for k, v in slack_user["private_channels"].items()])
+                logger.info(f"[INFO] {email} ({full_name}) is billing inactive (not logged-in since two weeks). "
+                            f"Person on private channels : {channels_names}")
 
     if is_inconsistent:
         raise Exception("Inconsistencies found, please fix them first")
+
+    for slack_email, slack_user in slack_users.items():
+        full_name = slack_user["profile"]["real_name"]
+        email = slack_email
+        if slack_user['is_invite_special']:
+            channels_names = ", ".join(
+                [v["name"] + " (" + v["id"] + ")" for k, v in slack_user["all_channels"].items()])
+            logger.info(f"[INFO] {email} ({full_name}) is invite special on slack on channels : {channels_names}")
 
     count_not_filled = 0
     count_filled = 0
@@ -322,7 +446,7 @@ def check_consistency(csv_users, slack_users):
         full_name = csv_user['fullname']
         slack_id = csv_user['slack_id']
         email = csv_user['email']
-        if csv_user['benevoles_channel']:
+        if csv_user['is_benevole']:
             if not is_filled(csv_user['fullname']) \
                     or not is_filled(csv_user['firstname']) \
                     or not is_filled(csv_user['lastname']) \
@@ -352,6 +476,6 @@ if __name__ == '__main__':
 
     check_consistency(csv_users, slack_users)
 
-    csv_users_filtered_list = (csv_user for csv_user in csv_users.values() if csv_user['benevoles_channel'])
+    csv_users_filtered_list = (csv_user for csv_user in csv_users.values() if csv_user['is_benevole'])
 
     to_json(csv_users_filtered_list, args.out_json, args.out_pics_folder)
